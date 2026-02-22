c823e45760069d80d8056bd6812b975975eef22d
Rishab Karki <nullrish@gmail.com>
2026-02-22 19:38:52 +0545

    Fixed the algorithm according to what leetcode prefers.

--- arrays/35_search-insert-position.cpp
+++ arrays/35_search-insert-position.cpp
@@ -3,6 +3,25 @@
 
 using namespace std;
 
+/*
+ * Logic for this algorithm.
+ * Constraint is we must do it in time complexity O(log n)
+ * For this we must use binary search and for that array must be sorted.
+ * So, we assume vector is always sorted here.
+ * For binary search:
+ * Initialize low = 0 and high = size of vector - 1;
+ * While low <= high:
+ *  mid = low + (high - low) / 2; We start from middle.
+ *  if (nums[mid] == target) // If mid is the target then we simply return mid;
+ *    return mid;
+ *
+ * If we found we returned mid (position) else we check,
+ * if (nums[mid] < target):
+ *  low = mid + 1; if target is greater then we know our target is bigger than
+ * middle value so we discard everything below mid. else high = mid - 1; else we
+ * discard everything above mid index and set it as next high.
+ * */
+
 class Solution {
 public:
   int searchInsert(vector<int> &nums, int target) {

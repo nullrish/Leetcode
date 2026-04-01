58258a9efb8c8cd123cce389eb9c4a38016bfd22
Rishab Karki <nullrish@gmail.com>
2026-04-01 18:17:31 +0545

    merge sorted arrays

---
+++ arrays/88_merge-sorted-array/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i := m - 1
+	j := n - 1
+	k := m + n - 1
+	for j >= 0 {
+		if i >= 0 && nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+}
+
+func main() {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 3, 4}
+
+	merge(nums1, 3, nums2, 3)
+	fmt.Println(nums1)
+}
